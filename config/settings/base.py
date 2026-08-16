06c89a61fe2c66b9c7d82afeb8f01d06ba88a886
MohamadKamardin1 <focusinzanzibar@outlook.com>
2026-08-16 06:44:29 +0300

    add some changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -100,10 +100,7 @@ MIDDLEWARE = [
 REQUEST_ID_HEADER = "HTTP_X_REQUEST_ID"
 
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", "OPTIONS": {"min_length": 12}},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+   
 ]
 
 TEMPLATES = [
